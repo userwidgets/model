ab63bfa0035cb8f21cf003db47478d1db2786996
Elias Eriksson <eliaseriksson95@gmail.com>
2022-09-05 16:56:54 +0200

    fixed user creatable is permission bug

--- User/Creatable.spec.ts
+++ User/Creatable.spec.ts
@@ -3,27 +3,22 @@ import * as model from "../index"
 describe("User.Creatable", () => {
 	const creatable: model.User.Creatable = {
 		email: "jane@example.com",
-		password: {
-			new: "asd",
-			repeat: "asd",
-		},
-		name: {
-			first: "jane",
-			last: "doe",
-		},
+		password: { new: "asd", repeat: "asd" },
+		name: { first: "jane", last: "doe" },
 		permissions: {
-			"*": {
-				application: {},
-				organization: {},
-				user: {},
-			},
-			acme: {
-				organization: {},
-				user: {},
-			},
+			"*": { application: {}, organization: {} },
+			acme: { organization: {}, user: { read: true } },
 		},
 	}
 	it("is", () => {
 		expect(model.User.Creatable.is(creatable)).toEqual(true)
+		expect(
+			model.User.Creatable.is({
+				email: "jane@example.com",
+				password: { new: "asd", repeat: "asd" },
+				name: { first: "jane", last: "doe" },
+				permissions: { acme: { organization: {}, user: {} } },
+			})
+		)
 	})
 })
