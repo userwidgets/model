d8b48e492b93b7f7dca60ac3efb8149cafcc29a9
linusarnejohansson <86045901+linusarnejohansson@users.noreply.github.com>
2022-11-11 11:27:23 +0100

    rework of password (#27)
    * rework of password
    
    * formatting

--- User/Password/Change.ts
+++ User/Password/Change.ts
@@ -1,5 +1,7 @@
+import * as cryptly from "cryptly"
+import * as gracely from "gracely"
+import type { Password } from "./index"
 import { Set } from "./Set"
-
 export interface Change extends Set {
 	old: string
 }
@@ -8,7 +10,22 @@ export namespace Change {
 	export function is(value: Change | any): value is Change & Record<string, any> {
 		return Set.is(value) && typeof value.old == "string"
 	}
-	export function validate(value: Change): boolean {
-		return value.new.length >= 6 && value.new == value.repeat && value.new != value.old
+
+	export async function verify(
+		password: Password | Change,
+		hash: cryptly.Password.Hash,
+		hashSecret?: string
+	): Promise<boolean | gracely.Error> {
+		return hashSecret
+			? await cryptly.Password.verify(
+					cryptly.Signer.create("HMAC", "SHA-512", hashSecret),
+					hash,
+					typeof password == "string" ? password : password.old
+			  )
+			: gracely.server.misconfigured("hashSecret", "hashSecret is not set in worker environment")
+	}
+
+	export async function validate(value: Change, hash: cryptly.Password.Hash, hashSecret?: string): Promise<boolean> {
+		return Set.validate(value) && (await verify(value, hash, hashSecret)) && value.new != value.old
 	}
 }
--- User/Password/index.ts
+++ User/Password/index.ts
@@ -1,12 +1,27 @@
+import * as cryptly from "cryptly"
+import * as gracely from "gracely"
 import { Change as PasswordChange } from "./Change"
 import { Set as PasswordSet } from "./Set"
 
-export type Password = PasswordChange | PasswordSet
+export type Password = string
 
 export namespace Password {
 	export function is(value: Password | any): value is (PasswordChange | PasswordSet) & Record<string, any> {
-		return PasswordSet.is(value) || PasswordChange.is(value)
+		return typeof value == "string"
 	}
+	export async function hash(password: Password, hashSecret?: string): Promise<cryptly.Password.Hash | gracely.Error> {
+		let result: cryptly.Password.Hash | gracely.Error
+		if (!hashSecret)
+			result = gracely.server.misconfigured("hashSecret", "hashSecret is not set in worker environment")
+		else
+			result = await cryptly.Password.hash(
+				cryptly.Signer.create("HMAC", "SHA-512", hashSecret),
+				password,
+				cryptly.RandomValue.generate(new Uint8Array(64)).toString()
+			)
+		return result
+	}
+	export const verify = PasswordChange.verify
 	export type Change = PasswordChange
 	export const Change = PasswordChange
 	export type Set = PasswordSet
