dd6bd2ae819c25390e3e039f03eebc4d8c4fe826
Elias Eriksson <eliaseriksson95@gmail.com>
2022-11-15 14:25:29 +0100

    implementation of copy and assign

--- User/Permissions/Readable.ts
+++ User/Permissions/Readable.ts
@@ -47,4 +47,36 @@ export namespace Readable {
 		)
 		return current
 	}
+	function nest<T extends Record<string, any>>(target: T, [head, ...tail]: string[], value: any): T {
+		return (
+			(target[head as keyof T] = tail.length
+				? nest(target[head] != undefined ? target[head] : (target[head as keyof T] = {} as T[keyof T]), tail, value)
+				: value),
+			target as T
+		)
+	}
+	export function copy(permissions: Readable, value?: boolean): Readable {
+		return Object.entries(permissions).reduce<Readable>((target, [id, permissions]) => {
+			permissions &&
+				Object.entries(permissions).forEach(
+					([resource, permission]) =>
+						permission &&
+						Object.entries(permission).forEach(([access, permitted]) =>
+							nest(target, [id, resource, access], arguments.length != 1 ? value : permitted)
+						)
+				)
+			return target
+		}, {})
+	}
+	export function assign(target: Readable, source: Readable): Readable {
+		return Object.entries(source).reduce<Readable>((target, [id, permissions]) => {
+			permissions &&
+				Object.entries(permissions).forEach(
+					([resource, access]) =>
+						access &&
+						Object.entries(access).forEach(([access, permitted]) => nest(target, [id, resource, access], permitted))
+				)
+			return target
+		}, target)
+	}
 }
