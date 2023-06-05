47ef29d67d7d61480d6a9eeeb5c8b7aa672b7b4b
Elias Eriksson <elias@issuefab.com>
2023-06-05 10:40:08 +0200

    Short invites (#41)
    * invite client
    
    * path fix

---
+++ ClientCollection/Me/Invite.ts
@@ -0,0 +1,13 @@
+import { gracely } from "gracely"
+import { http } from "cloudly-http"
+import * as rest from "cloudly-rest"
+import type { userwidgets } from "../../index"
+
+export class Invite extends rest.Collection<gracely.Error> {
+	constructor(client: http.Client, readonly configuration: userwidgets.Configuration) {
+		super(client)
+	}
+	async fetch(id: string): Promise<string | gracely.Error> {
+		return this.client.get<string>(`${this.configuration.pathPrefix}/me/invite/${id}`)
+	}
+}
