0996e7718ac430467c07d1720d2f62b23b5dee4b
Elias Eriksson <eliaseriksson95@gmail.com>
2023-07-04 15:39:49 +0200

    interface

--- User/Feedback/Notification/index.ts
+++ User/Feedback/Notification/index.ts
@@ -2,15 +2,15 @@ import { gracely } from "gracely"
 import { isly } from "isly"
 import { Notification as NotificationInterface } from "./Notification"
 
-export type Notification = NotificationInterface | gracely.Error
+export type Notification = Notification.Interface | gracely.Error
 
 export namespace Notification {
-	export const type = isly.union<Notification, NotificationInterface, gracely.Error>(
+	export type Interface = NotificationInterface
+	export const Interface = NotificationInterface
+	export const type = isly.union<Notification, Interface, gracely.Error>(
 		NotificationInterface.type,
 		isly.fromIs("gracely.Error", gracely.Error.is)
 	)
 	export const is = type.is
 	export const flaw = type.flaw
-	export type Interface = NotificationInterface
-	export const Interface = NotificationInterface
 }
