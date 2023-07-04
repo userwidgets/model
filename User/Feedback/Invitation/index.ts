3eeccff0c1c8530faf2b2c03cbd89b8bf954198b
Elias Eriksson <eliaseriksson95@gmail.com>
2023-07-04 15:38:34 +0200

    invitation interface

--- User/Feedback/Invitation/index.ts
+++ User/Feedback/Invitation/index.ts
@@ -2,14 +2,14 @@ import { gracely } from "gracely"
 import { isly } from "isly"
 import { Invitation as InvitationInterface } from "./Invitation"
 
-export type Invitation = InvitationInterface | gracely.Error
+export type Invitation = Invitation.Interface | gracely.Error
 export namespace Invitation {
-	export const type = isly.union<Invitation, InvitationInterface, gracely.Error>(
+	export type Interface = InvitationInterface
+	export const Interface = InvitationInterface
+	export const type = isly.union<Invitation, Interface, gracely.Error>(
 		InvitationInterface.type,
 		isly.fromIs("gracely.Error", gracely.Error.is)
 	)
 	export const is = type.is
 	export const flaw = type.flaw
-	export type Interface = InvitationInterface
-	export const Interface = InvitationInterface
 }
