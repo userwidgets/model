6390c6915da32c664ea2d683d15bd53289a725ad
Elias Eriksson <eliaseriksson95@gmail.com>
2023-07-06 17:04:39 +0200

    changable

--- User/Changeable.ts
+++ User/Changeable.ts
@@ -1,13 +1,17 @@
 import { isly } from "isly"
-import type { User } from "./index"
+import { User } from "./index"
 import { Name } from "./Name"
 
 export interface Changeable {
 	name?: User["name"]
+	password?: User.Password.Change
+	permissions?: any
 }
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: Name.type.optional(),
+		password: User.Password.Change.type.optional(),
+		permissions: isly.any(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
