73389590c33087f210cc32b6b1a769b47b439e72
Elias Eriksson <eliaseriksson95@gmail.com>
2023-07-31 09:53:56 +0200

    draft

--- User/Permissions/index.ts
+++ User/Permissions/index.ts
@@ -1,12 +1,119 @@
+import { Organization } from "../../Organization"
 import { Application as PermissionsApplication } from "./Application"
 import { Collection as PermissionsCollection } from "./Collection"
 import { Organization as PermissionsOrganization } from "./Organization"
 import { Permission as PermissionsPermission } from "./Permission"
 import { Readable as PermissionsReadable } from "./Readable"
-
-export interface Permissions {
-	[applicationId: string]: Permissions.Readable | undefined
+// extensive
+type Permissions = {
+	// users permissions on organization in current application
+	[id: Organization["id"]]:
+		| {
+				// user resource permissions for this organization
+				user?:
+					| {
+							// allow viewing users in this orgs.
+							// should this just be the default always?
+							view?: true
+							// allow editing users in this orgs.
+							// OBS! These changes will reflect across all orgs!
+							// maybe should not exist
+							// maybe password change?
+							edit?: true
+							// allow invite of new users to this org.
+							invite?: true
+					  }
+					| true
+				// organization resource permissions for this organization
+				org?:
+					| {
+							// allow viewing of all details of this org.
+							// this would mostly include viewing available permissions?
+							// maybe this is unnecessary and should come from having edit on this level
+							view?: true
+							// allow editing details of this org
+							// includes removing users. should removing user be another permission?
+							edit?: true
+					  }
+					| true
+				// extend with own resources with custom format
+		  }
+		| undefined
+} & {
+	// permissions on current application user is logged into
+	"*"?: {
+		// administrative user resource permissions
+		user?:
+			| {
+					// allow viewing users even outside of own orgs.
+					view?: true
+					// allow editing of all users this user can view
+					edit?: true
+					// allow invite of new users to all viewable orgs
+					invite?: true
+			  }
+			| true
+		// administrative organization resource permissions
+		org?:
+			| {
+					// view all orgs across this app
+					view?: true
+					// allow editing of all orgs this user can view
+					edit?: true
+					// allow creation of new organizations to this app
+					create?: true
+			  }
+			| true
+		// maybe this is unnecessary as there is nothing implemented to support this.
+		// skip for now and implement later if needed?
+		app?: {
+			// view all applications on userwidgets
+			view?: true
+			// edit all applications on userwidgets
+			edit?: true
+			// create new applications on userwidgets
+			create?: true
+		}
+		//extend with custom resources with custom formats
+	}
 }
+
+// minimal
+// description of permissions exists in the long version
+/*
+type Permissions = {
+	[id: Organization.Identifier]: {
+		user?: {
+			invite?: true
+		}
+		org?:
+			| {
+					edit?: true
+			  }
+			| true
+	}
+} & {
+	"*": {
+		user?:
+			| {
+					view?: true
+					edit?: true
+					invite?: true
+			  }
+			| true
+		org?:
+			| {
+					view?: true
+					edit?: true
+					create?: true
+			  }
+			| true
+	}
+}
+*/
+// export interface Permissions {
+// 	[applicationId: string]: Permissions.Readable | undefined
+// }
 
 export namespace Permissions {
 	export function is(value: Permissions | any): value is Permissions {
