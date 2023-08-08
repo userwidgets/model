acfde13f6559aba7835c36e14adefc59d829070e
Linus Johansson <linus@issuefab.com>
2023-08-08 10:54:55 +0200

    invite interface

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -2,9 +2,16 @@ import { isly } from "isly"
 import { Email } from "../Email"
 import type { Organization } from "./index"
 
+// This is an object because it is going to have the invitees permissions on it as well. 
+// Permissions is being worked on in another branch and will be added by that branch when it merges
+interface Invite {
+	user: Email
+	//permissions
+}
+
 export interface Changeable {
 	name?: Organization["name"]
-	users?: Email[]
+	users?: (Email | Invite)[]
 	permissions?: Organization["permissions"]
 }
 export namespace Changeable {
