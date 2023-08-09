b746b7ec3dd89f6976b654ec3c5b4e89057c23e6
Linus Johansson <linus@issuefab.com>
2023-08-09 08:34:30 +0200

    invite interface

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -2,23 +2,29 @@ import { isly } from "isly"
 import { Email } from "../Email"
 import type { Organization } from "./index"
 
-// This is an object because it is going to have the invitees permissions on it as well. 
+// This is an object because it is going to have the invitees permissions on it as well.
 // Permissions is being worked on in another branch and will be added by that branch when it merges
-interface Invite {
-	user: Email
-	//permissions
-}
 
 export interface Changeable {
 	name?: Organization["name"]
-	users?: (Email | Invite)[]
+	users?: (Email | Changeable.Invite)[]
 	permissions?: Organization["permissions"]
 }
 export namespace Changeable {
+	export interface Invite {
+		user: Email
+		//permissions
+	}
+	export namespace Invite {
+		export const type = isly.object<Invite>({
+			user: Email.type,
+			//permissions
+		})
+	}
 	export const type = isly.object<Changeable>({
 		name: isly.string(/.+/).optional(),
 		permissions: isly.array(isly.string(/.+/)).optional(),
-		users: isly.array(Email.type).optional(),
+		users: isly.array(isly.union(Invite.type, Email.type)).optional(), //update this
 	})
 	export const is = type.is
 	export const flaw = type.flaw
