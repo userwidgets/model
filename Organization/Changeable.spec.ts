d80a0a4e77e095b73f9700aff30f4ed101764a06
Linus Johansson <linus@issuefab.com>
2023-08-09 09:15:16 +0200

    formatting tests

--- Organization/Changeable.spec.ts
+++ Organization/Changeable.spec.ts
@@ -21,19 +21,22 @@ describe("Organization.Changeable", () => {
 		expect(userwidgets.Organization.Changeable.is({ name: "" })).toEqual(false)
 		expect(userwidgets.Organization.Changeable.is({ permissions: ["foo", ""] })).toEqual(false)
 		expect(userwidgets.Organization.Changeable.is({ name: 1 })).toEqual(false)
-		expect(userwidgets.Organization.Changeable.Invite.is({ user: "James@example.com" })).toEqual(true)
-		expect(
-			userwidgets.Organization.Changeable.is({ users: [{ user: "James@example.com" }, "name@example.com"] })
-		).toEqual(true)
-		expect(userwidgets.Organization.Changeable.is({ users: [{ user: 1 }, "name@example.com"] })).toEqual(false)
-		expect(
-			userwidgets.Organization.Changeable.is({ users: [{ name: "James@example.com" }, "name@example.com"] })
-		).toEqual(false)
 	})
 
 	it("get", () => {
 		expect(userwidgets.Organization.Changeable.type.get({ ...organization, created: isoly.Date.now() })).toEqual(
 			organization
 		)
+	})
+	const invite: userwidgets.Organization.Changeable.Invite = {
+		user: "James@example.com",
+	}
+	it("invite is", () => {
+		expect(userwidgets.Organization.Changeable.Invite.is(invite)).toEqual(true)
+		expect(userwidgets.Organization.Changeable.is({ users: [invite, "name@example.com"] })).toEqual(true)
+		expect(userwidgets.Organization.Changeable.is({ users: [{ user: 1 }, "name@example.com"] })).toEqual(false)
+		expect(
+			userwidgets.Organization.Changeable.is({ users: [{ name: "James@example.com" }, "name@example.com"] })
+		).toEqual(false)
 	})
 })
