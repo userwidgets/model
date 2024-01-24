de7872428d7a4948f3ddee16fca347789392da7d
Anton Kroner <anton.kroner@pax2pay.com>
2024-01-24 16:56:54 +0100

    Added twofactor to user changeable.

--- User/Changeable.ts
+++ User/Changeable.ts
@@ -7,12 +7,14 @@ export interface Changeable {
 	name?: User["name"]
 	password?: User.Password.Change | User.Password.Set
 	permissions?: User["permissions"]
+	twoFactor?: User["twoFactor"]
 }
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: Name.type.optional(),
 		password: isly.union(Password.Change.type, Password.Set.type).optional(),
 		permissions: isly.string().optional(),
+		twoFactor: isly.string().optional(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
